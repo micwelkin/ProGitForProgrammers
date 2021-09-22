1f8a9e1e40cf4f4cfbd30f4f5a2f9c0726fe65ed
micwelkin <82009983+micwelkin@users.noreply.github.com>
2021-09-23 00:48:13 +0800

    Edite in VS to Add a class for VS tutorial

--- Program.cs
+++ Program.cs
@@ -10,3 +10,10 @@ namespace ProGitForProgrammers
 			}
 		}
 }
+
+public class Book
+{
+	public string Title {get; set;}
+	public List<string> Authors {get;set;}
+	public DateTime PublicationDate {get; set;}
+}
